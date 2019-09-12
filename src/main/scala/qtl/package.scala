195971aeaaad23d096494dc3aba78bc40a014e82
khavelun <klaus.havelund@jpl.nasa.gov>
2019-09-12 12:04:31 -0700

    updated rules doc

---
+++ src/main/scala/qtl/package.scala
@@ -0,0 +1,12 @@
+
+/**
+ * The QTL (Quantified Temporal Logic) DSL is effectively the
+ * [[https://github.com/havelund/dejavu DejaVu]] logic. It is in F' included
+ * only as a jar file. See the website above for further documentation.
+ *
+ * DejaVu is capable of online monitoring but is originally script-wise set up for log file analysis.
+ * Incorporating DejaVu into an F' component is doable at a prototyping level, but the process can be
+ * improved.
+ */
+
+package object qtl {}
